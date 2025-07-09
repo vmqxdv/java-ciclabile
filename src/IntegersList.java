a7b00b79f1c62992c67d9ff555caa2d4f4433f0c
vmqxdv <vquintilio@gmail.com>
2025-07-09 17:28:45 +0200

    feat: implement empty constructor and addElement method

--- src/IntegersList.java
+++ src/IntegersList.java
@@ -3,6 +3,10 @@ public class IntegersList {
   private int[] integers;
   private int index;
 
+  public IntegersList() {
+    this.integers = new int[0];
+  }
+
   public IntegersList(int[] arr) {
     this.integers = arr;
   }
@@ -14,5 +18,17 @@ public class IntegersList {
 
   public boolean hasNextElement() {
     return index + 1 < integers.length;
+  }
+
+  public void addElement(int num) {
+    int[] newIntegers = new int[this.integers.length + 1];
+
+    for (int i = 0; i < this.integers.length; i++) {
+      newIntegers[i] = this.integers[i];
+    }
+
+    newIntegers[newIntegers.length - 1] = num;
+
+    this.integers = newIntegers;
   }
 }
